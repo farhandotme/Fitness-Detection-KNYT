9573f0bc3cddbff94adc646652aa8730df544db8
farhandotme <princeofficial355@gmail.com>
2026-07-15 17:21:32 +0530

    updated the routes

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -8,8 +8,9 @@ from src.routes.pushupRoutes import router as pushupRouter
 from src.routes.jumpingJackRoutes import router as jumpingJackRouter
 from src.routes.lungeRoutes import router as lungeRouter
 from src.routes.highKneesRoutes import router as highKneesRouter
-from src.routes.mountainClimberRoutes import router as mountainClimberRouter
+from src.routes.plankRoutes import router as plankRouter
 from src.routes.bodyAnalysisRoutes import router as bodyAnalysisRouter
+from src.routes.mountainClimberRoutes import router as mountainClimberRouter
 
 app = FastAPI()
 
@@ -33,5 +34,6 @@ app.include_router(pushupRouter, prefix="/ws", tags="pushup")
 app.include_router(jumpingJackRouter, prefix="/ws", tags="jumping-jack")
 app.include_router(lungeRouter, prefix="/ws", tags="lunge")
 app.include_router(highKneesRouter, prefix="/ws", tags="high-knees")
+app.include_router(plankRouter, prefix="/ws", tags="plank-hold")
 app.include_router(mountainClimberRouter, prefix="/ws", tags="mountain-climber")
 app.include_router(bodyAnalysisRouter, prefix="/api", tags="body-analysis")
