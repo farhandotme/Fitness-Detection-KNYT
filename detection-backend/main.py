9b404016af1aa6e0b488bcff022364c6c4c46de4
farhandotme <princeofficial355@gmail.com>
2026-07-16 13:08:24 +0530

    created the routes

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -11,6 +11,7 @@ from src.routes.highKneesRoutes import router as highKneesRouter
 from src.routes.plankRoutes import router as plankRouter
 from src.routes.bodyAnalysisRoutes import router as bodyAnalysisRouter
 from src.routes.mountainClimberRoutes import router as mountainClimberRouter
+from src.routes.shoulderPressRoutes import router as shoulderPressRouter
 
 app = FastAPI()
 
@@ -37,3 +38,4 @@ app.include_router(highKneesRouter, prefix="/ws", tags="high-knees")
 app.include_router(plankRouter, prefix="/ws", tags="plank-hold")
 app.include_router(mountainClimberRouter, prefix="/ws", tags="mountain-climber")
 app.include_router(bodyAnalysisRouter, prefix="/api", tags="body-analysis")
+app.include_router(bodyAnalysisRouter, prefix="/ws", tags="shoulder-press")
