cecc2d815d41255e541141039572aedf54a98c4e
farhandotme <princeofficial355@gmail.com>
2026-07-25 11:30:54 +0530

    butt kicks socket connection done

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -34,6 +34,7 @@ from src.routes.armCirclesRoutes import router as armCirclesRouter
 from src.routes.standingCrossCrunchRoutes import router as standingCrossCrunchRouter
 from src.routes.peacockPoseRoutes import router as peacockPoseRouter
 from src.routes.hollowHoldRoutes import router as hollowHoldRouter
+from src.routes.buttKicksRoutes import router as buttKicksRouter
 from src.routes.flutterKicksRoutes import router as flutterKicksRouter
 from src.routes.windmillRotationStretchRoutes import (
     router as windmillRotationStretchRouter,
@@ -85,6 +86,7 @@ app.include_router(armCirclesRouter, prefix="/ws", tags="arm-circles-router")
 app.include_router(peacockPoseRouter, prefix="/ws", tags="peacock-pose-router")
 app.include_router(hollowHoldRouter, prefix="/ws", tags="hollow-hold-router")
 app.include_router(flutterKicksRouter, prefix="/ws", tags="flutter-kicks-router")
+app.include_router(buttKicksRouter, prefix="/ws", tags="butt-kicks-router")
 app.include_router(
     windmillRotationStretchRouter, prefix="/ws", tags="windmill-rotation-stretch-router"
 )
