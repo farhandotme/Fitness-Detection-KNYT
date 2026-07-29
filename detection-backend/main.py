9e88a47934f4b0e9acb3297d9174261c35527085
farhandotme <princeofficial355@gmail.com>
2026-07-29 10:30:46 +0530

    routes defined of supermen pose

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -44,6 +44,7 @@ from src.routes.bicycleCrunchRoutes import router as bicycleCrunchRouter
 from src.routes.hinduPushupRoutes import router as hinduPushupRouter
 from src.routes.arnoldPressRoutes import router as arnoldPressRouter
 from src.routes.dancerPoseRoutes import router as dancerPoseRouter
+from src.routes.supermanRoutes import router as supermenPoseRouter
 from src.routes.windmillRotationStretchRoutes import (
     router as windmillRotationStretchRouter,
 )
@@ -104,6 +105,7 @@ app.include_router(bicycleCrunchRouter, prefix="/ws", tags="bicycle-crunch-route
 app.include_router(hinduPushupRouter, prefix="/ws", tags="hindu-pushup-router")
 app.include_router(arnoldPressRouter, prefix="/ws", tags="arnold-press-router")
 app.include_router(dancerPoseRouter, prefix="/ws", tags="dancer-pose-router")
+app.include_router(supermenPoseRouter, prefix="/ws", tags="supermen-pose-router")
 app.include_router(
     windmillRotationStretchRouter, prefix="/ws", tags="windmill-rotation-stretch-router"
 )
