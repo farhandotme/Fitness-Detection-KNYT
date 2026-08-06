616b815e2d2b2474f1743c35d9cbf66175804ce5
farhandotme <princeofficial355@gmail.com>
2026-08-06 16:19:02 +0530

    Add Standing Trunk Rotation routes: implement routing for standing trunk rotation analysis

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -59,6 +59,9 @@ from src.routes.full_body.inchwormRoutes import router as inchwormRouter
 from src.routes.upper_body.bentOverRowRoutes import router as bentOverRowRouter
 from src.routes.full_body.shoulderStandRoutes import router as shoulderStandRouter
 from src.routes.upper_body.tricepDipRoutes import router as tricepDipRouter
+from src.routes.upper_body.standingTrunkRotationRoutes import (
+    router as standingTrunkRotationRouter,
+)
 from src.routes.upper_body.frontRaiseRoutes import router as frontRaiseRouter
 from src.routes.upper_body.seatedCableShrugRoutes import (
     router as seatedCableShrugRouter,
@@ -136,6 +139,9 @@ app.include_router(bentOverRowRouter, prefix="/ws", tags="bent-over-row-router")
 app.include_router(shoulderStandRouter, prefix="/ws", tags="shoulder-stand-router")
 app.include_router(frontRaiseRouter, prefix="/ws", tags="front-raise-router")
 app.include_router(tricepDipRouter, prefix="/ws", tags="tricep-dip-router")
+app.include_router(
+    standingTrunkRotationRouter, prefix="/ws", tags="standing-trunk-rotation-router"
+)
 app.include_router(
     seatedCableShrugRouter, prefix="/ws", tags="seated-cable-shrug-router"
 )
