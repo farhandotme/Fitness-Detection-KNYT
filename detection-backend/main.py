a5b51fbe84fab0ecf1cf124ff8b9973b3ab41369
farhandotme <princeofficial355@gmail.com>
2026-08-08 16:30:00 +0530

    Add Skipping exercise routes: include WebSocket endpoint for skipping exercise

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -69,6 +69,7 @@ from src.routes.cardio.skierJumpingJacksRoutes import router as skierJumpingJack
 from src.routes.cardio.battleRopeRoutes import router as battleRopeRouter
 from src.routes.cardio.lineHopRoutes import router as lineHopRouter
 from src.routes.cardio.kneeStrikeRoutes import router as kneeStrikeRouter
+from src.routes.full_body.skippingRoutes import router as skippingRouter
 
 from src.routes.upper_body.reverseFlyDumbellRoutes import (
     router as reverseFlyDumbellRouter,
@@ -172,6 +173,7 @@ app.include_router(squatJacksRouter, prefix="/ws", tags="squat-jacks-router")
 app.include_router(battleRopeRouter, prefix="/ws", tags="battle-rope-router")
 app.include_router(lineHopRouter, prefix="/ws", tags="line-hop-router")
 app.include_router(kneeStrikeRouter, prefix="/ws", tags="knee-strike-router")
+app.include_router(skippingRouter, prefix="/ws", tags="skipping-router")
 app.include_router(
     reverseFlyDumbellRouter, prefix="/ws", tags="reverse-fly-dumbell-router"
 )
