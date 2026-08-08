cda5a5616f5addd79e7d6cbcf18229a44bfeecd1
farhandotme <princeofficial355@gmail.com>
2026-08-08 15:32:05 +0530

    Add Reverse Fly Dumbbell routes: include routing for reverse fly dumbbell exercise in the FastAPI application

--- detection-backend/main.py
+++ detection-backend/main.py
@@ -69,6 +69,11 @@ from src.routes.cardio.skierJumpingJacksRoutes import router as skierJumpingJack
 from src.routes.cardio.battleRopeRoutes import router as battleRopeRouter
 from src.routes.cardio.lineHopRoutes import router as lineHopRouter
 from src.routes.cardio.kneeStrikeRoutes import router as kneeStrikeRouter
+
+from src.routes.upper_body.reverseFlyDumbellRoutes import (
+    router as reverseFlyDumbellRouter,
+)
+
 from src.routes.lower_body.bulgarianSplitSquatRoutes import (
     router as bulgarianSplitSquatRouter,
 )
@@ -167,6 +172,10 @@ app.include_router(squatJacksRouter, prefix="/ws", tags="squat-jacks-router")
 app.include_router(battleRopeRouter, prefix="/ws", tags="battle-rope-router")
 app.include_router(lineHopRouter, prefix="/ws", tags="line-hop-router")
 app.include_router(kneeStrikeRouter, prefix="/ws", tags="knee-strike-router")
+app.include_router(
+    reverseFlyDumbellRouter, prefix="/ws", tags="reverse-fly-dumbell-router"
+)
+
 app.include_router(
     skierJumpingJacksRouter, prefix="/ws", tags="skier-jumping-jacks-router"
 )
